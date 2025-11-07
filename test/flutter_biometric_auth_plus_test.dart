90e89a8b0da4659cab25c202843224722cb9ea1b
Aina <rajoelisonainatiavina@gmail.com>
2025-11-07 18:27:58 +0300

    test: add mock for platform interface
    - Add `MockBiometricAuthAdvancedPlatform`
    - Implement all methods of `BiometricAuthAdvancedPlatform`
    - Fix typo in test description

--- test/flutter_biometric_auth_plus_test.dart
+++ test/flutter_biometric_auth_plus_test.dart
@@ -10,12 +10,47 @@ class MockBiometricAuthAdvancedPlatform
 
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<bool> canCheckBiometrics() => Future.value(false);
+
+  @override
+  Future<List<String>> getAvailableBiometrics() => Future.value(<String>[]);
+
+  @override
+  Future<bool> hasEnrolledBiometrics() => Future.value(false);
+
+  @override
+  Future<bool> canAuthenticateWithBiometricsStrong() => Future.value(false);
+
+  @override
+  Future<bool> canAuthenticateWithBiometricsWeak() => Future.value(false);
+
+  @override
+  Future<bool> canAuthenticateWithDeviceCredential() => Future.value(false);
+
+  @override
+  Future<Map<String, dynamic>> authenticate({
+    required String title,
+    String? subtitle,
+    String? description,
+    String negativeButtonText = 'Cancel',
+    bool confirmationRequired = false,
+    bool allowDeviceCredential = false,
+    String biometricStrength = 'strong',
+  }) => Future.value(<String, dynamic>{});
+
+  @override
+  Future<Map<String, dynamic>> getBiometricInfo() => Future.value(<String, dynamic>{});
+
+  @override
+  Future<void> cancelAuthentication() => Future.value();
 }
 
 void main() {
   final BiometricAuthAdvancedPlatform initialPlatform = BiometricAuthAdvancedPlatform.instance;
 
-  test('$MethodChannelBiometricAuthAdvanced is the default instance', () {
+  test('MethodChannelBiometricAuthAdvanced is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelBiometricAuthAdvanced>());
   });
 
